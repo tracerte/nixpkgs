0641d7ba43a0a2f2e480ff4aa1c22e92793e2451
Matthew B. Reisch <dev@tracerte.com>
2020-10-05 12:15:32 -0400

    Added auto startx to zshrc and switched to a leaner version of powerlevel10K

--- dotfiles/zshrc.nix
+++ dotfiles/zshrc.nix
@@ -6,6 +6,8 @@ let
 in
   pkgs.writeText "zshrc" ''
 
+if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then exec startx; fi
+
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block, everything else may go below.
